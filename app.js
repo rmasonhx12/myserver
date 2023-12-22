1e55a8010864c84aabee399de0450e35dd538e45
Robert Hendricks <rh12@email.sc.edu>
2023-12-21 22:55:38 -0500

    create simple router

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 // build koa server
 
 const Koa = require('koa');
-
+const KoaRouter = require('koa-router');
 const json = require('koa-json');
 
 const port = 3100;
 const app = new Koa();
+const router = new KoaRouter();
 
 app.use(json());
 // simple middleware
 
 //app.use(async ctx =>(ctx.body = 'Welcome to Jacqueline Taylor and Friends'));
-app.use(async ctx =>(ctx.body = {Text:'Welcome to Jacqueline Taylor and Friends'}));
+//app.use(async ctx =>(ctx.body = {Text:'Welcome to Jacqueline Taylor and Friends'}));
+
+
+router.get('/home', ctx => {
+    ctx.body = {Text:'Welcome to Jacqueline Taylor and Friends'};
+});
+
+// router middleware
+app.use(router.routes()).use(router.allowedMethods());
 
 
 
