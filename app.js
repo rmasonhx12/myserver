cd3f145c78887dcc942fc57fe7c3fdc557434c00
Robert Hendricks <rh12@email.sc.edu>
2023-12-21 22:24:46 -0500

    Initial KOA Server with simple middleware

---
+++ app.js
@@ -0,0 +1,14 @@
+// build koa server
+
+const Koa = require('koa');
+
+const port = 3100;
+const app = new Koa();
+
+
+// simple middleware
+
+app.use(async ctx =>(ctx.body = 'Welcome to Jacqueline Taylor and Friends'));
+
+
+app.listen(port, () => {console.log(`Server is running on port ${port}`)});
